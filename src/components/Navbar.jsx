8624240d362708430f2af9170542d9e8b752125a
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-30 00:02:11 -0700

    create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div className="bg-neutral">
+      <div className="max-w-5xl mx-auto">
+        <div className="navbar bg-neutral text-neutral-content">
+          <button className="btn btn-ghost text-xl">Retro-Cars</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
