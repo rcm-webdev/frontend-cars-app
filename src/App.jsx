8aaf8f2de4be3f0928f878b70dd6062fcbe8182f
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-30 13:48:21 -0700

    add scroll component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import "./App.css";
 import Carousel from "./components/Carousel";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-
+import Footer from "./components/Footer";
+import Timeline from "./components/Timeline";
+import ScrollDown from "./components/ScrollDown";
 function App() {
   return (
     <div className="bg-base-200">
       <Navbar />
       <Hero />
+      <ScrollDown />
+      <Timeline />
       <Carousel />
+      <Footer />
     </div>
   );
 }
