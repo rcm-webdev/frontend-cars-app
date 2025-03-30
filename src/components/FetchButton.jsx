bda5553e2259a65dbe705cabd1f7d170ea51590f
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-30 00:34:54 -0700

    update fetch btn component

--- src/components/FetchButton.jsx
+++ src/components/FetchButton.jsx
@@ -1,7 +1,7 @@
 function FetchButton() {
   return (
     <button className="btn btn-primary shadow-2xl flex flex-row-reverse gap-2 items-center">
-      <span class="loading loading-dots loading-xl"></span>
+      <span className="loading loading-dots loading-xl"></span>
       <span>Button</span>
     </button>
   );
