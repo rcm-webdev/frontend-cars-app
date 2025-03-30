a40857a900b6b0f1034e17d5f4c468dd9ae871db
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-30 00:01:12 -0700

    create hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import HeroImage from "../../public/hero.webp";
+import FetchButton from "./FetchButton";
+
+function Hero() {
+  return (
+    <div className="max-w-5xl mx-auto bg-base-200">
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={HeroImage} className="max-w-sm rounded-lg " />
+          <div>
+            <h1 className="text-5xl font-bold">The Collection</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <FetchButton />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
