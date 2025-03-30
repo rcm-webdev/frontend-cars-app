79b1675954a3903f3f3632861a1b2cd32c4eb937
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-30 13:49:31 -0700

    update logo name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
+import FetchButton from "./FetchButton";
+
 function Navbar() {
   return (
     <div className="bg-neutral">
       <div className="max-w-5xl mx-auto">
-        <div className="navbar bg-neutral text-neutral-content">
-          <button className="btn btn-ghost text-xl">Retro-Cars</button>
+        <div className="navbar bg-neutral text-neutral-content justify-between">
+          <button className="btn btn-ghost text-3xl font-cooper tracking-wide uppercase">
+            <span className="text-[#5d9277]">509</span> Vintage Cars
+          </button>
+          <FetchButton />
         </div>
       </div>
     </div>
